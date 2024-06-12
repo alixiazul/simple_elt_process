bf45bdb2380cc5a5758d6d59c5a2dfc043e88ee5
alixiazul <alicia.rodriguez.t@gmail.com>
2024-06-12 14:44:39 +0100

    Show retrying message correctly. Load environment variables from files .env.source and .env.destination so the code does not show real values.

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -8,7 +8,7 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
     while retries < max_retries:
         try:
             result = run(
-                ["pg_isready", "-h", host], check=True, catpure_output=True,
+                ["pg_isready", "-h", host], check=True, capture_output=True,
                 text=True)
             if "accepting connections" in result.stdout:
                 print("Sucessfully connected to Postgres")
@@ -17,38 +17,39 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
             print(f"Error connecting to Postgres: {e}")
             retries += 1
             print(
-                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/",
-                f"{max_retries}"
+                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})"
             )
             sleep(delay_seconds)
-        print("Max retries reached. Exiting")
-        return False
+    print("Max retries reached. Exiting")
+    return False
 
 
-if not wait_for_postgres(host="source_postgres"):
+# Load variables from source database configuration file
+source_config = dotenv_values("../.env.source")
+
+# Load variables from destination database configuration file
+destination_config = dotenv_values("../.env.destination")
+
+db_host = source_config['POSTGRES_HOST']
+
+if not wait_for_postgres(host=db_host):
     exit(1)
 
 print("Starting ELT script...")
 
-# Load variables from source database configuration file
-source_config = dotenv_values(".env.source")
-
-# Load variables from destination database configuration file
-destination_config = dotenv_values(".env.destination")
-
 # Using dump files to initialise the database
 dump_command = [
     'pg_dump',
-    '-h', source_config['HOST'],
-    '-u', source_config['USER'],
-    '-d', source_config['DB_NAME'],
+    '-h', source_config['POSTGRES_HOST'],
+    '-U', source_config['POSTGRES_USER'],
+    '-d', source_config['POSTGRES_DB'],
     '-f', 'data_dump.sql',
     '-w'  # no prompt for password
 ]
 
 # Set environment variables to avoid the password dump
 # It will NOT ask for the password every single time
-subprocess_env = dict(PGPASSWORD=source_config['password'])
+subprocess_env = dict(PGPASSWORD=source_config['POSTGRES_PASSWORD'])
 
 # Execute the dump_command created previously to dump the sql file
 # into the source database.
@@ -57,15 +58,15 @@ run(dump_command, env=subprocess_env, check=True)
 # Get everything from the source database to the destination database
 load_command = [
     'psql',
-    '-h', destination_config['HOST'],
-    '-u', destination_config['USER'],
-    '-d', destination_config['DB_NAME'],
+    '-h', destination_config['POSTGRES_HOST'],
+    '-U', destination_config['POSTGRES_USER'],
+    '-d', destination_config['POSTGRES_DB'],
     '-a', '-f', 'data_dump.sql'  # print all lines from the file
 ]
 
 # Set environment variables to avoid the password dump
 # It will NOT ask for the password every single time
-subprocess_env = dict(PGPASSWORD=destination_config['password'])
+subprocess_env = dict(PGPASSWORD=destination_config['POSTGRES_PASSWORD'])
 
 # Execute the load_command created previously to load the sql file
 # into the destination database.
